4af2d4a8da805f8b5f2d88c4496d706d964cfa92
Olabanji Victor <Olabanjivictor37@gmail.com>
2022-11-28 20:28:35 +0100

    Removal of the Line Chart form the dashboard

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,6 +1,5 @@
 
 import React from 'react';
-import { Line } from 'react-chartjs-2';
 import { Col, Row, Typography } from 'antd';
 
 const { Title } = Typography;
@@ -50,7 +49,7 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
           <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
         </Col>
       </Row>
-      <Line data={data} options={options} />
+      {/* <Line data={data} options={options} /> */}
     </>
   );
 };
